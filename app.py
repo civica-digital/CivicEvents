16bbfa885065b1ad6a9491ef22ad5a6de29cfed8
Ricardo Alanis <ricardo.alanis@gmail.com>
2016-09-29 14:15:46 -0500

    Scrap unused endpoints from template

--- app.py
+++ app.py
@@ -32,29 +32,6 @@ else:
 # Routing for your application.
 ###
 
-@app.route('/')
-def home():
-    """Render website's home page."""
-    return render_template('home.html')
-
-
-@app.route('/about/')
-def about():
-    """Render the website's about page."""
-    return render_template('about.html')
-
-
-###
-# The functions below should be applicable to all Flask apps.
-###
-
-@app.route('/<file_name>.txt')
-def send_text_file(file_name):
-    """Send your static text file."""
-    file_dot_text = file_name + '.txt'
-    return app.send_static_file(file_dot_text)
-
-
 @app.route('/event')
 def event():
     """Saves an event on the database."""
